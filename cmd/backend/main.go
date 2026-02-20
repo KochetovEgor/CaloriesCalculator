3bf2e5cc95e8627b843f9fc14071fd19eff896e6
Egor Kochetov <egorkochetov02@gmail.com>
2026-02-20 03:38:56 +0300

    Added mylog package

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"CaloriesCalculator/internal/config"
+	"CaloriesCalculator/internal/mylog"
 	"fmt"
 	"log"
+	"log/slog"
+	"os"
 )
 
 const congigBackendPath = "config/backend.json"
 
 func main() {
-	fmt.Println("Hello world")
+	mylog.InitLogger(os.Stdout)
+	slog.Info("logger initialized")
+
 	cfg, err := config.LoadConfig(congigBackendPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+	slog.Info("configs loaded")
+
 	fmt.Println(cfg.Server.Address)
 }
--- internal/mylog/mylog.go
+++ internal/mylog/mylog.go
@@ -1,7 +1,29 @@
 package mylog
 
-import "fmt"
+import (
+	"context"
+	"io"
+	"log/slog"
+)
 
-func Test() {
-	fmt.Println("test")
+func InitLogger(w io.Writer) {
+	var handler slog.Handler
+	handler = slog.NewJSONHandler(w, &slog.HandlerOptions{Level: slog.LevelDebug})
+	slog.SetDefault(slog.New(handler))
+}
+
+type contextKey int
+
+const loggerKey contextKey = 0
+
+func NewContext(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, logger)
+}
+
+func FromContext(ctx context.Context) *slog.Logger {
+	logger, ok := ctx.Value(loggerKey).(*slog.Logger)
+	if ok {
+		return logger
+	}
+	return slog.Default()
 }
