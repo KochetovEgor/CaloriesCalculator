21b866ecd51c8bf2c5755390d8eea6d0fd4200f6
Egor Kochetov <egorkochetov02@gmail.com>
2026-03-14 03:16:46 +0300

    Changed signature of responces with error from http controller.

--- internal/controller/http/utils.go
+++ internal/controller/http/utils.go
@@ -1,11 +1,17 @@
 package http
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
 )
 
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
 func errorWithLog(w http.ResponseWriter, err string, code int, logger *slog.Logger) {
 	logger.Debug(err, "status code", code)
-	http.Error(w, err, code)
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(errorResponse{Error: err})
 }
